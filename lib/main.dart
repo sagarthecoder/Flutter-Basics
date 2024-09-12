4c14e586ca61797b2252e4f3736cb24506005e62
Sagar Chandra Das <sagarthecoder@gmail.com>
2024-09-12 12:04:04 +0600

    created clickable Icon and modified color

--- lib/main.dart
+++ lib/main.dart
@@ -5,19 +5,38 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('I am poor 2'),
-          backgroundColor: Colors.blue[900],
-          centerTitle: true,
-        ),
-        backgroundColor: Colors.green,
-        body: const Center(
-          child: Image(
-            image: AssetImage('images/diamond.png'),
-          ),
-        ),
-      ),
+      home: Home(),
     ),
   );
+}
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('I am poor 2'),
+        backgroundColor: Colors.blue[900],
+        centerTitle: true,
+      ),
+      backgroundColor: Colors.green,
+      body: Center(
+        child: IconButton(
+            onPressed: () {
+              print("you clicked me");
+            },
+            color: Colors.red,
+            icon: Icon(Icons.mail)),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          print("Pressed");
+        },
+        child: const Icon(Icons.add),
+        backgroundColor: Colors.pink.shade300,
+      ),
+    );
+  }
 }
